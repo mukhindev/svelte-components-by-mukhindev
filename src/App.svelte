385de870fa870ace171236453aeeea388c9e3834
mukhindev <mukhin.dev@gmail.com>
2019-11-12 23:51:01 +0300

    Demo

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,28 @@
 <script>
 import InputMoveControl from './components/InputMoveControl.svelte'
 
-const config = {
-  min: 0,
-  max: 100
-}
+let valueInput = 120
 </script>
 
-<InputMoveControl {...config}/>
+<style>
+.wrap {
+  height: 100vh;
+  width: 100vw;
+}
+
+.center {
+  margin: auto;
+}
+
+.unselectable {
+  user-select: none;
+}
+
+</style>
+
+<div class="wrap d-flex align-items-center">
+  <div class="center col-2">
+    <InputMoveControl bind:value={valueInput}/>
+    <p class="mt-3 unselectable">Результат из компонента: {valueInput}</p>
+  </div>
+</div>
--- src/components/InputMoveControl.svelte
+++ src/components/InputMoveControl.svelte
@@ -1,28 +1,104 @@
 <script>
-export let min
-export let max
+export let value
+
+let control = 0
+let offset = 0
+let last = value
+let active = ''
 
 function down () {
-  let e = window.event
-  console.log('Нажал:', e.y)
-  onmouseup = up
+  control = 1
+  last = value
+  offset = event.y
+  if (offset !== 0) {
+    value = last + -event.y + offset
+  } else {
+    value = last + -event.y
+  }
+  active = 'active'
+  onmousemove = move
+}
+
+function move () {
+  if (control === 1) {
+    if (offset !== 0) {
+      value = last + -event.y + offset
+    } else {
+      value = last + -event.y
+    }
+    onmouseup = up
+    document.body.style.cursor = "ns-resize";
+  }
 }
 
 function up () {
-  let e = window.event
-  console.log('Отжал:', e.y)
+  if (control === 1) { 
+    if (offset !== 0) {
+      value = last + -event.y + offset
+    } else {
+      value = last + -event.y
+    }
+    last = value
+  }
+  control = 0
+  active = ''
+  document.body.style.cursor = "default";
 }
 </script>
 
 <style>
-.box {
-  width: 200px;
-  height: 200px;
-  background-color: blanchedalmond;
+.position1 {
+  position: absolute;
+  top: 50%;
+  width: 12px;
+  height: 36px;
+  right: 0;
+}
+.position2 {
+  position: absolute;
+  background-color: rgb(255, 255, 255);
+  top: -50%;
+  width: 20px;
+  height: 36px;
+  right: 8px;
+}
+.position2:before {
+  content: '';
+  position: absolute;
+  width: inherit;
+  height: 38px;
+  top: -1px;
+  cursor: ns-resize;
+}
+.marker {
+  right: -8px;
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  border-radius: 100%;
+  background-color: #C4DFFE;
+  transition: 0.25s;
+  transform: scale(0.25);
+  cursor: ns-resize;
+}
+.marker.active {
+  transform: scale(0.5);
+  transition: 0.25s;
+}
+.relative {
+  position: relative;
+}
+.unselectable {
+  user-select: none;
 }
 </style>
 
-<pre class="box" on:mousedown={down}>
-min: {min}
-max: {max}
-</pre>
+<div class="relative">
+  <input type="number" bind:value={value} value="{value}" class="form-control">
+  <div class="position1">
+    <div on:mousedown={down} class="position2">
+      <div class="marker unselectable {active}">
+      </div>
+    </div>
+  </div>
+</div>
