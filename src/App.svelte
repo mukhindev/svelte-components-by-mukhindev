c68ba037ac900a071bb440092baf9e0261f384a3
mukhindev <mukhin.dev@gmail.com>
2019-11-12 17:07:20 +0300

    Init

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+import InputMoveControl from './components/InputMoveControl.svelte'
+
+const config = {
+  min: 0,
+  max: 100
+}
+</script>
+
+<InputMoveControl {...config}/>
---
+++ src/components/InputMoveControl.svelte
@@ -0,0 +1,28 @@
+<script>
+export let min
+export let max
+
+function down () {
+  let e = window.event
+  console.log('Нажал:', e.y)
+  onmouseup = up
+}
+
+function up () {
+  let e = window.event
+  console.log('Отжал:', e.y)
+}
+</script>
+
+<style>
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: blanchedalmond;
+}
+</style>
+
+<pre class="box" on:mousedown={down}>
+min: {min}
+max: {max}
+</pre>
